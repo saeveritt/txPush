2dd525260ff8e35e3b139d5e6310b637cdf681c5
Peerchemist <dx.1178@gmail.com>
2017-08-05 14:14:01 +0200

    further improvements.

--- tx_push/app.py
+++ tx_push/app.py
@@ -1,15 +1,27 @@
+from sys import argv
 from flask import Flask, jsonify
 from peercoin_rpc import Client
-
-node = Client(testnet=True)
 
 app = Flask(__name__)
 
 
 @app.route('/sendrawtransaction/<txid>')
 def sendtx(txid):
+
+    assert isinstance(txid, str), {'error': 'we only take strings around here.'}
+
     result = node.sendrawtransaction(txid)
     return jsonify(result)
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5555)
+
+    if argv[1] == "-testnet":
+        node = Client(testnet=True)
+        print("Running in testnet mode.")
+        app.run(host='0.0.0.0', port=5555)
+
+    else:
+        node = Client(testnet=False)
+        print("Running in mainnet mode.")
+        app.run(host='0.0.0.0', port=5555)
