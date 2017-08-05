bb181c45f444a1a5ac8306d7e6be48a81cd8ccc6
Peerchemist <dx.1178@gmail.com>
2017-08-05 14:24:14 +0200

    start setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='tx_push',
+      version='0.1',
+      description='Simple Flask based sendrawtransaction push API.',
+      keywords=['peerassets', 'blockchain', 'assets', 'api'],
+      url='https://github.com/peerassets/tx_push',
+      author=('Peerchemist', 'Saeveritt'),
+      license='BSD2',
+      packages=['tx_push'],
+      install_requires=['peercoin_rpc', 'flask']
+      )
